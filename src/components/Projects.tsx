0552c55d1ea1625df5cce71145370108adfe292f
Mohamed Alaa <muhamed.a.rashed@gmail.com>
2025-09-21 00:42:29 +0300

    update v5

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -325,37 +325,37 @@ const Projects: React.FC = () => {
                               )
                             )}
                           </div>
+                          <p className="text-sm text-gray-300 mt-2 flex items-center gap-2">
+                            <MessageCircleWarning
+                              size={26}
+                              className="inline-block icon-pulse"
+                              style={{ ["--primary-color" as any]: "#CB877C" }}
+                            />
+                            <span>
+                              This storyboard is a sample version for
+                              demonstration purposes only. All rights reserved.
+                            </span>
+                            <style>{`
+        .icon-pulse {
+          display: inline-block;
+          color: white;
+          transform-origin: center;
+          animation: iconPulse 1.6s ease-in-out infinite;
+        }
+
+        .icon-pulse svg {
+          transform-origin: center;
+        }
+
+        @keyframes iconPulse {
+          0%   { transform: scale(1);    color: white; }
+          50%  { transform: scale(1.2); color: var(--primary-color); }
+          100% { transform: scale(1);    color: white; }
+        }
+      `}</style>
+                          </p>
                         </>
                       )}
-                    <p className="text-sm text-gray-300 mt-2 flex items-center gap-2">
-                      <MessageCircleWarning
-                        size={26}
-                        className="inline-block icon-pulse"
-                        style={{ ["--primary-color" as any]: "#CB877C" }}
-                      />
-                      <span>
-                        This storyboard is a sample version for demonstration
-                        purposes only. All rights reserved.
-                      </span>
-                      <style>{`
-    .icon-pulse {
-      display: inline-block;
-      color: white;
-      transform-origin: center;
-      animation: iconPulse 1.6s ease-in-out infinite;
-    }
-
-    .icon-pulse svg {
-      transform-origin: center;
-    }
-
-    @keyframes iconPulse {
-      0%   { transform: scale(1);    color: white; }
-      50%  { transform: scale(1.2); color: var(--primary-color); }
-      100% { transform: scale(1);    color: white; }
-    }
-  `}</style>
-                    </p>
                   </div>
                 </div>
               </div>
